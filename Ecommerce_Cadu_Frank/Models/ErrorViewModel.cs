ee5ebff14a82df9c5058d9c86c14d7c105646f82
Francisco Alexandre <francisco.matematica.fisica@gmail.com>
2025-05-06 18:53:24 -0300

    Primeiro commit com .gitignore configurado

---
+++ Ecommerce_Cadu_Frank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_Cadu_Frank.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
